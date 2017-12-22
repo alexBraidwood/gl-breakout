1acdb39add2388224281dd13da1514ab18e9e3d1
Alex Braidwood <alex.braidwood@thebatforge.com>
2017-12-21 19:15:47 -0500

    Added build

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * File: main.cpp.
+ * Created by: Alex Braidwood.
+ * Date: Dec 21, 2017.
+ * Notice: Copyright (c) 2017 The Bat Forge. All Rights Reserved.
+ */
+
+#include <alexlib.h>
+
+int main(int argc, char* argv[]) {
+    alexlib::initialize();
+    alexlib::start();
+    return 0;
+}
